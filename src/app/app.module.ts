8b811dd1a8e9dd1d2ec1256018126b14f92f7a78
saospina <sergos0617@gmail.com>
2018-12-03 23:39:08 -0500

    creating the structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { PendientesPage } from '../pages/pendientes/pendientes.component';
+import { TerminadosPage } from '../pages/terminados/terminados.component';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    TabsPage,
+    PendientesPage,
+    TerminadosPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    TabsPage,
+    PendientesPage,
+    TerminadosPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { ContactPage } from '../contact/contact';
+import { PendientesPage } from '../pendientes/pendientes.component';
+import { TerminadosPage } from '../terminados/terminados.component';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = PendientesPage;
+  tab2Root = TerminadosPage;
+  tab3Root = ContactPage;
+
+  constructor() {
+
+  }
+}
