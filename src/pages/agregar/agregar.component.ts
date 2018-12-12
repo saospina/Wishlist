80c19386396714c0cb21cd9e6662d8110fbfd1d2
saospina <sergos0617@gmail.com>
2018-12-11 23:33:05 -0500

    Funcionalidad de la pantalla de agregar lista

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,6 +1,6 @@
 import { DeseosService } from '../../providers/deseos.service';
 import { Component } from '@angular/core';
-import { Lista } from '../../models';
+import { Lista,ListaItem } from '../../models';
 import { NavParams } from 'ionic-angular';
 
 @Component({
@@ -10,6 +10,7 @@ import { NavParams } from 'ionic-angular';
 export class AgregarPage {
 
     lista: Lista;
+    nombreItem: string = '';
     constructor(public deseosService: DeseosService,
         private navParams: NavParams) {
         const titulo = this.navParams.get('title');
@@ -18,6 +19,21 @@ export class AgregarPage {
 
 
 
+    }
+    agregarItem(){
+        this.nombreItem = this.nombreItem.trim();
+        if (this.nombreItem.length === 0) {
+            return;
+            
+        }
+        const newItem = new ListaItem(this.nombreItem);
+        this.lista.items.push(newItem);
+        this.nombreItem = '';
+
+        
+    }
+    actualizarTarea(item:ListaItem){
+        item.completado = !item.completado;
     }
 
 
